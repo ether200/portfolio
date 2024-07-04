dade7eb603db4ed8a11d75adc11ca94e27152b7c
Ivan Muntian <ivanmuntian@Ivans-MBP-2.fibertel.com.ar>
2024-07-04 11:19:22 -0300

    [NEW] Installed vercel analytics and removed favicon

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,15 @@ import "@fontsource/archivo/600.css";
 import "@fontsource/open-sans/400.css";
 import "@fontsource/open-sans/700.css";
 import "../styles/globals.scss";
+import { Analytics } from "@vercel/analytics/react"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <Analytics />
+    <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
